9c05e6bde4bb04843752e1bd865066c815f2d122
Sourabh Kumar Janghel <skj@Sourabhs-MBP.domain.name>
2022-04-20 21:42:03 +0530

    modify code allowed hosts

--- homelane/settings.py
+++ homelane/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://homelane-skj.herokuapp.com","0.0.0.0","localhost"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
